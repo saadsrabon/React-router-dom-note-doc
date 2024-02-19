9b0dd790af05160cbd5db8c547f552c1e3261023
Saad Srabon <saadsrabon2@gmail.com>
2024-02-19 16:27:15 +0600

    2024-02-19 16:27:15 - outlet added

--- src/main.jsx
+++ src/main.jsx
@@ -7,13 +7,21 @@ import {
 import "./index.css";
 import Root from "./routes/root";
 import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root/>,
-    errorElement:<ErrorPage/>
+    errorElement:<ErrorPage/>,
+    children:[
+      {
+        path: "/contacts/:contactId",
+        element: <Contact/>,
+        }
+    ]
   },
+  
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
