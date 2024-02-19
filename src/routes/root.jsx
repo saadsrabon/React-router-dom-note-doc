6e1abee48306006a455055ac6f505212a09eca8e
Saad Srabon <saadsrabon2@gmail.com>
2024-02-19 17:55:39 +0600

    2024-02-19 17:55:39 - RR Form added

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,6 +1,17 @@
-import { Link, Outlet } from "react-router-dom";
+import { Form, Link, Outlet ,useLoaderData } from "react-router-dom";
+import { createContact, getContacts } from "../Contact";
 
+export async function loader() {
+    const contacts = await getContacts();
+    return { contacts };
+  }
+  export async function action() {
+    const contact = await createContact();
+    return { contact };
+  }
+  
 export default function Root() {
+    const { contacts } = useLoaderData();
     return (
       <>
         <div id="sidebar">
@@ -24,19 +35,33 @@ export default function Root() {
                 aria-live="polite"
               ></div>
             </form>
-            <form method="post">
+            <Form method="post">
               <button type="submit">New</button>
-            </form>
+            </Form>
           </div>
           <nav>
+          {contacts.length ? (
             <ul>
-              <li>
-                <Link to={`/contacts/1`}>Your Name</Link>
-              </li>
-              <li>
-                <Link to={`/contacts/2`}>Your Friend</Link>
-              </li>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <Link to={`contacts/${contact.id}`}>
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}{" "}
+                    {contact.favorite && <span>★</span>}
+                  </Link>
+                </li>
+              ))}
             </ul>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
           </nav>
         </div>
         <div id="detail"><Outlet/></div>
